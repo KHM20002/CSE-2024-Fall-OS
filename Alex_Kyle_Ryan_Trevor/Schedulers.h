818cae72aba462c17493bff27f0304b5eb1f3aa1
KHM20002 <kyle.madsen@uconn.edu>
2024-11-14 23:11:01 -0500

    Update data struct, sort arrival time, FIFO, and RR to  Schedulers.h

--- Alex_Kyle_Ryan_Trevor/Schedulers.h
+++ Alex_Kyle_Ryan_Trevor/Schedulers.h
@@ -1,1 +1,19 @@
+#ifndef SCHEDULERS_H  
+#define SCHEDULERS_H
 
+struct process {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int waiting_time;
+    int turnaround_time;
+    int completion_time;
+    int remaining_time;
+    int is_completed;
+};
+
+void sort_by_arrival_time(struct process processes[], int n);
+void FIFO_Scheduling(struct process processes[], int n);
+void RR_Scheduling(struct process processes[], int n, int time_quantom);
+
+#endif
